61edc4c045d8e2a9d2bb52675f30ddc905e4d9e3
Andrea Amantini <lo.zampino@gmail.com>
2022-12-09 18:44:34 +0100

    Yarn install

--- build/build.clj
+++ build/build.clj
@@ -1,14 +1,15 @@
 (ns build
   (:require [shadow.cljs.devtools.api :as shadow]
             [babashka.fs :as fs]
+            [clojure.java.shell :refer [sh]]
             [nextjournal.clerk :as clerk]
             [nextjournal.clerk.config :as config]))
 
 (defn build! [opts]
+  (sh "yarn" "install")
   (shadow/release! :viewer)
   (let [cas
         (nextjournal.clerk.viewer/store+get-cas-url! {:out-path "public/build" :ext "js"}
                                                      (fs/read-all-bytes "public/build/viewer.js"))]
-
     (swap! config/!resource->url assoc "/js/viewer.js" cas)
     (clerk/build! (merge {:paths ["notebooks/**"]} opts))))
