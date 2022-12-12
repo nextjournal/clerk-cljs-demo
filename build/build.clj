a3e50d4f016e275ae577c80f99f4924c30c64404
Andrea Amantini <lo.zampino@gmail.com>
2022-12-12 14:51:50 +0100

    Pass resource map as argument for builder

--- build/build.clj
+++ build/build.clj
@@ -1,15 +1,16 @@
 (ns build
-  (:require [shadow.cljs.devtools.api :as shadow]
-            [babashka.fs :as fs]
-            [clojure.java.shell :refer [sh]]
-            [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.config :as config]))
+  (:require
+   [babashka.fs :as fs]
+   [clojure.java.shell :refer [sh]]
+   [nextjournal.clerk :as clerk]
+   [nextjournal.clerk.viewer :as clerk.viewer]
+   [shadow.cljs.devtools.api :as shadow]))
 
 (defn build! [opts]
   (sh "npm" "install")
   (shadow/release! :viewer)
-  (let [js-resource-url
-        (nextjournal.clerk.viewer/store+get-cas-url! (assoc opts :ext "js")
-                                                     (fs/read-all-bytes "public/build/viewer.js"))]
-    (swap! config/!resource->url assoc "/js/viewer.js" js-resource-url)
-    (clerk/build! (merge {:paths ["notebooks/**"]} opts))))
+  (let [js-resource-url (clerk.viewer/store+get-cas-url! (assoc opts :ext "js")
+                                                         (fs/read-all-bytes "public/build/viewer.js"))]
+    (clerk/build! (-> opts
+                      (assoc :paths ["notebooks/**"])
+                      (update :resource->url assoc "/js/viewer.js" js-resource-url)))))
