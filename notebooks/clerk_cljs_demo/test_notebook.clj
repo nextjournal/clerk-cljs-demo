324e6ad70e528da88ac5bb25b886ea6ebece8652
Sam Ritchie <sritchie09@gmail.com>
2022-12-15 15:42:50 -0700

    repro

--- notebooks/clerk_cljs_demo/test_notebook.clj
+++ notebooks/clerk_cljs_demo/test_notebook.clj
@@ -1,7 +1,31 @@
 (ns clerk-cljs-demo.test-notebook
   (:require [nextjournal.clerk :as clerk]))
 
-(clerk/with-viewer
- '(fn [_ _] [:h1 clerk-cljs-demo.viewers-lib/header]) nil)
+^{::clerk/sync true}
+(defonce !state
+  (atom 4))
 
-;; and much more...
+(clerk/with-viewer
+  '(fn [_ _]
+     (defn ClientSlider [!state]
+       [:<>
+        [:div
+         [:input
+          {:type :range :min 0 :max 10 :step 1
+           :value @!state
+           :on-change
+           (fn [target]
+             (let [v (.. target -target -value)]
+               (reset! !state (js/parseInt v))))}]
+         " " @!state]])
+
+     [:<>
+      [:p "This cljs-defined slider will NOT update the server:"]
+      [demo/CompiledSlider clerk-cljs-demo.test-notebook/!state]
+      [:p "This sci-defined slider will successfully update the server:"]
+      [ClientSlider clerk-cljs-demo.test-notebook/!state]])
+  nil)
+
+;; Server state:
+
+@!state
--- src/clerk_cljs_demo/sci_env_ext.cljs
+++ src/clerk_cljs_demo/sci_env_ext.cljs
@@ -4,9 +4,24 @@
             [clerk-cljs-demo.viewers-lib]
             [sci.core :as sci]))
 
+(defn CompiledSlider
+  "Component that takes an atom with an integer and updates it with a slider."
+  [!state]
+  [:<>
+   [:div
+    [:input
+     {:type :range :min 0 :max 10 :step 1
+      :value @!state
+      :on-change
+      (fn [target]
+        (let [v (.. target -target -value)]
+          (reset! !state (js/parseInt v))))}]
+    " " @!state]])
+
 (def extra-namespaces
   {:namespaces
-   {'clerk-cljs-demo.viewers-lib
+   {'demo {'CompiledSlider CompiledSlider}
+    'clerk-cljs-demo.viewers-lib
     (sci/copy-ns clerk-cljs-demo.viewers-lib
                  (sci/create-ns 'clerk-cljs-demo.viewers-lib))}})
 
